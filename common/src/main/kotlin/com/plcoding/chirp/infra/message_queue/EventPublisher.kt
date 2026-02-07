dfefbb671b350fccec70a3e0fa2ff56111a74a7c
antoan <antoandonchev29@gmail.com>
2026-02-07 14:11:28 +0200

    common folder fixed with missed files

--- common/src/main/kotlin/com/plcoding/chirp/infra/message_queue/EventPublisher.kt
+++ common/src/main/kotlin/com/plcoding/chirp/infra/message_queue/EventPublisher.kt
@@ -1,7 +1,27 @@
 package com.plcoding.chirp.infra.message_queue
 
-import com.plcoding.chirp.domain.events.user.UserEvent
+import com.plcoding.chirp.domain.events.ChirpEvent
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Component
 
-class EventPublisher {
-    fun publish(event: UserEvent.Created) {}
+@Component
+class EventPublisher(
+    private val rabbitTemplate: RabbitTemplate
+) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun <T: ChirpEvent> publish(event: T) {
+        try {
+            rabbitTemplate.convertAndSend(
+                event.exchange,
+                event.eventKey,
+                event
+            )
+            logger.info("Successfully published event: ${event.eventKey}")
+        } catch(e: Exception) {
+            logger.error("Failed to publish ${event.eventKey} event", e)
+        }
+    }
 }
