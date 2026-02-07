7c1d8a36a56e0300157bd032cd4ed5e77c9a7227
antoan <antoandonchev29@gmail.com>
2026-02-07 14:15:14 +0200

    notification folder fixed

--- notification/src/main/kotlin/com/plcoding/chirp/infra/message_queue/NotificationUserEventListener.kt
+++ notification/src/main/kotlin/com/plcoding/chirp/infra/message_queue/NotificationUserEventListener.kt
@@ -1,4 +1,43 @@
 package com.plcoding.chirp.infra.message_queue
 
-class NotificationUserEventListener {
+import com.plcoding.chirp.domain.events.user.UserEvent
+import com.plcoding.chirp.service.EmailService
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Component
+import java.time.Duration
+
+@Component
+class NotificationUserEventListener(private val emailService: EmailService) {
+
+    @RabbitListener(queues = [MessageQueues.NOTIFICATION_USER_EVENTS])
+    fun handleUserEvent(event: UserEvent) {
+        when(event) {
+            is UserEvent.Created -> {
+                emailService.sendVerificationEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.verificationToken
+                )
+            }
+            is UserEvent.RequestResendVerification -> {
+                emailService.sendVerificationEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.verificationToken
+                )
+            }
+            is UserEvent.RequestResetPassword -> {
+                emailService.sendPasswordResetEmail(
+                    email = event.email,
+                    username = event.username,
+                    userId = event.userId,
+                    token = event.passwordResetToken,
+                    expiresIn = Duration.ofMinutes(event.expiresInMinutes)
+                )
+            }
+            else -> Unit
+        }
+    }
 }
