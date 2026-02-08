60d6512a714c8597138ec4450519729508c86e1b
antoan <antoandonchev29@gmail.com>
2026-02-08 23:31:08 +0200

    adding and leaving chat

---
+++ chat/src/main/kotlin/com/plcoding/chirp/api/controllers/ChatController.kt
@@ -0,0 +1,55 @@
+package com.plcoding.chirp.api.controllers
+
+import com.plcoding.chirp.api.dto.AddParticipantToChatDto
+import com.plcoding.chirp.api.dto.ChatDto
+import com.plcoding.chirp.api.dto.CreateChatRequest
+import com.plcoding.chirp.api.mappers.toChatDto
+import com.plcoding.chirp.domain.type.ChatId
+import com.plcoding.chirp.service.ChatService
+import com.plcoding.chirp.util.requestUserId
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/chat")
+class ChatController(
+    private val chatService: ChatService
+) {
+
+    @PostMapping
+    fun createChat(
+        @Valid @RequestBody body: CreateChatRequest
+    ): ChatDto {
+        return chatService.createChat(
+            creatorId = requestUserId,
+            otherUserIds = body.otherUserIds.toSet()
+        ).toChatDto()
+    }
+
+    @PostMapping("/{chatId}/add")
+    fun addChatParticipants(
+        @PathVariable chatId: ChatId,
+        @Valid @RequestBody body: AddParticipantToChatDto
+    ): ChatDto{
+        return chatService.addParticipantsToChat(
+            requestUserId = requestUserId,
+            chatId = chatId,
+            userIds = body.userIds.toSet()
+        ).toChatDto()
+    }
+
+    @DeleteMapping("/{chatId}/leave")
+    fun leaveChat(
+        @PathVariable chatId: ChatId,
+    ){
+         chatService.removeParticipantFromChat(
+            chatId = chatId,
+            userId = requestUserId
+        )
+    }
+}
---
+++ chat/src/main/kotlin/com/plcoding/chirp/domain/exception/ChatParticipantNotFoundException.kt
@@ -0,0 +1,9 @@
+package com.plcoding.chirp.domain.exception
+
+import com.plcoding.chirp.domain.type.UserId
+
+class ChatParticipantNotFoundException(
+    private val id: UserId
+): RuntimeException(
+    "The chat participant with the ID $id was not found"
+)
